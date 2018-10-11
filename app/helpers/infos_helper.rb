4cbfe17119c9620a841c3fba8d7ec32e60ce5286
mtaka <mtakadev@gmail.com>
2018-10-11 15:34:11 +0900

    Added Info->NameValuePairs for expressing Customer Profile

--- app/helpers/infos_helper.rb
+++ app/helpers/infos_helper.rb
@@ -3,6 +3,8 @@ module InfosHelper
     case info.data_type
     when 'FromToPairs'
       raw build_pairs_div_from_lines(info.data, %w|from to|).join
+    when 'NameValuePairs'
+      raw build_pairs_div_from_lines(info.data, %w|name value|).join
     else
       info.data
     end
